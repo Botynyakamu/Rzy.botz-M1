0aec201531133a91dc9ac8df14d5d157bf0405c8
AlyaaXzy <96462315+AlyaaXd@users.noreply.github.com>
2022-02-06 02:08:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 let fs = require('fs')
-global.owner = ['6281333154367', '0','628998512588', '6283175998566', '6285693602003', '6283153189868'] // Letakan nomor kamu disini
+global.owner = ['6285730903853', '6282221792667','6285730903853', '6282221792667', '6285730903853', '6282221792667'] // Letakan nomor kamu disini
 global.APIs = { // API Prefix
   // nama: 'https://website'
   hardianto: 'https://hardianto-chan.herokuapp.com',
@@ -40,7 +40,7 @@ global.author = 'NyyXz'
 global.wait = '「 ⏱️ 」Harap tunggu...'
 global.eror = '「❗」Server ERROR!'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
-global.watermark = '© Exc' //change the watermark 
+global.watermark = '©AlyaaXzy X BOTTZ' //change the watermark 
 global.image = 'https://telegra.ph/file/1a3eac3701775793618b3.jpg' //change the image
 global.thumbfoto = 'https://telegra.ph/file/39bbded9693c9338069fd.jpg'
 
