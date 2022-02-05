5cad2fa1c0ee90599b6411d6b7602526935e2859
AlyaaXzy <96462315+AlyaaXd@users.noreply.github.com>
2022-02-06 04:40:56 +0700

    Update darkjoke.js

--- plugins/darkjoke.js
+++ plugins/darkjoke.js
@@ -1,3 +1,5 @@
+// RECODE ALYAAXZY
+
 let fetch = require('node-fetch')
 let handler  = async (m, { conn, usedPrefix, command }) => {
 conn.sendButtonImg(m.chat, await ( await fetch(global.API('xteam', '/asupan/darkjoke', {}, 'APIKEY'))).buffer(), 'Drag joles', watermark, 'NEXT', `${usedPrefix + command}`, m)
