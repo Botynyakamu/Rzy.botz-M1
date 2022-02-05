2d4d8d2ecc90406a16851e8eac035995381c85f8
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2022-02-06 05:37:42 +0700

    Snooze

--- plugins/snk.js
+++ plugins/snk.js
@@ -4,15 +4,15 @@ let handler = async(m, { conn }) => {
 Kebijakan Privasi, Syarat Ketentuan dan Peraturan Haruno Bot
 
 Kebijakan Privasi
-1. Harunobot tidak akan merekam data riwayat chat user.
-2. Harunobot tidak akan menyebarkan nomor users.
-3. Harunobot tidak akan menyimpan media yang dikirimkan oleh users.
-4. Harunobot tidak akan menyalah gunakan data data users.
-5. Owner Harunobot berhak melihat data riwayat chat users.
-6. Owner Harunobot berhak melihat status users.
-7. Owner Harunobot dapat melihat riwayat chat, dan media yang dikirimkan users.
+1. R-Txzy tidak akan merekam data riwayat chat user.
+2. R-Txzy tidak akan menyebarkan nomor users.
+3. R-Txzy tidak akan menyimpan media yang dikirimkan oleh users.
+4. R-Txzy tidak akan menyalah gunakan data data users.
+5. Owner R-Txzy berhak melihat data riwayat chat users.
+6. Owner R-Txzy berhak melihat status users.
+7. Owner R-Txzy dapat melihat riwayat chat, dan media yang dikirimkan users.
 
-Peraturan Haruno Bot
+Peraturan R-Txzy Bot
 1. Users dilarang menelpon maupun memvideo call nomor bot.
 2. Users dilarang mengirimkan berbagai bug, virtex, dll ke nomor bot.
 3. Users diharap tidak melakukan spam dalam penggunaan bot.
@@ -21,15 +21,15 @@ Peraturan Haruno Bot
 
 Syarat Ketentuan Haruno Bot
 1. Bot akan keluar dari group apabila sudah waktunya keluar.
-2. Harunobot dapan mem-ban users secara sepihak terlepas dari users salah atau tidak.
-3. Harunobot *tidak akan bertanggungjawab atas apapun yang users lakukan terhadap fitur bot.*
-4. Harunobot akan memberlakukan hukuman: block atau ban terhadap users yang melanggar peraturan.
-5. Harunobot bertanggung jawab atas kesalahan fatal dalam programing maupun owner.
+2. R-Txzy dapan mem-ban users secara sepihak terlepas dari users salah atau tidak.
+3. R-Txzy *tidak akan bertanggungjawab atas apapun yang users lakukan terhadap fitur bot.*
+4. R-Txzy akan memberlakukan hukuman: block atau ban terhadap users yang melanggar peraturan.
+5. R-Txzy bertanggung jawab atas kesalahan fatal dalam programing maupun owner.
 
--Haruno
--Fadli
+- AlyaXd
+- TioXd
 
-Peraturan: 1 Oktober 2021
+Peraturan: 1 Februaru 2022
 `.trim()
     conn.send2ButtonLoc(m.chat, await(await fetch(image)).buffer(), teks, watermark, 'Menu', '.menu', 'Owner', '.owner', m)
 }
