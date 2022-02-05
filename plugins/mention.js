b9e491e7b4906d5c9d2ca02bdb020d208e6861b5
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2022-02-06 03:18:15 +0700

    Mention Update ngentot

--- plugins/mention.js
+++ plugins/mention.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) throw `contoh:\n${usedPrefix + command} @6285157336614`
+  if (!text) throw `contoh:\n${usedPrefix + command} @6282221792667`
   m.reply(text, false, {
     contextInfo: {
       mentionedJid: conn.parseMention(text)
