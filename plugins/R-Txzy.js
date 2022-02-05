1fe189df4364756d018bb67faec66927faa6a6d0
AlyaaXzy <96462315+AlyaaXd@users.noreply.github.com>
2022-02-06 02:26:16 +0700

    Create R-Txzy.js

---
+++ plugins/R-Txzy.js
@@ -0,0 +1,25 @@
+// Recode AlyaaXzy
+
+let handler  = async (m, { conn, usedPrefix: _p }) => {
+ye = `@${m.sender.split`@`[0]}`
+let info = `Hai Kak ${ye} SC ini hasil dari developer AlyaaXzy X BOTCAH Terimakasih sudah memakai bot kami
+`.trim()
+
+conn.fakeReply(m.chat, info, '0@s.whatsapp.net', `*${global.packname}*`, 'status@broadcast') 
+}
+handler.help = ['github']
+handler.tags = ['info']
+handler.command = ['script', 'sc', 'scbot', 'github']
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.exp = 25
+
+module.exports = handler
