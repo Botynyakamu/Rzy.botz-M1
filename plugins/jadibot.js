e281d9e2faf6c01a426a45920502afd3cf0eb277
Botynyakamu <102163283+Botynyakamu@users.noreply.github.com>
2022-03-31 02:44:57 +0700

    Update jadibot.js

--- plugins/jadibot.js
+++ plugins/jadibot.js
@@ -69,7 +69,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
   } else throw 'Tidak bisa membuat bot didalam bot!\n\nhttps://wa.me/' + global.conn.user.jid.split`@`[0] + '?text=.jadibot'
 }
 handler.help = ['jadibot']
-handler.tags = ['jadibot', 'premium']
+handler.tags = ['jadibot']
 
 handler.command = /^jadibot$/i
 handler.premium = true
