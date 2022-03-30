6e5ab899afcab619ad5c59dec99d2fa43f627d51
Botynyakamu <102163283+Botynyakamu@users.noreply.github.com>
2022-03-31 02:11:54 +0700

    Update totalfitur.js

--- plugins/totalfitur.js
+++ plugins/totalfitur.js
@@ -1,10 +1,10 @@
-// RECODE ALYAAXZY
+// RECODE Rzy-botz
 
 let handler = async (m, { conn, args, command }) => {
 let totalf = Object.values(global.plugins).filter(
     (v) => v.help && v.tags
   ).length;
-conn.reply(m.chat, `Total Fitur dari Botz R-Txzy saat ini: ${totalf}`,m)
+conn.reply(m.chat, `Total Fitur dari Rzy-bitz saat ini: ${totalf}`,m)
 }
 
 handler.help = ['totalfitur']
